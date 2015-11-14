63b5d7fd4ac2e1d21b086ad05254b6ad1f078386
earthshine0 <earthshinelynx@gmail.com>
2015-11-13 21:31:53 -0800

    added message encryption and decryption into main screen

--- TauNet.py
+++ TauNet.py
@@ -3,7 +3,8 @@ from os import urandom
 
 password = str.encode('password')
 REPS = 20
-message = str.encode("This is a message")
+#message = str.encode("This is a message")
+
 
 def swap(l, a, b):
     temp = 1[a]
@@ -12,7 +13,7 @@ def swap(l, a, b):
 
 
 def rc4(message, password):
-    i=0
+    i = 0
     state = list(range(256))
     key = list(range(256))
     cipher = b""
@@ -21,39 +22,40 @@ def rc4(message, password):
         while i < 256:
             ctmp = password[i % len(password):(i % len(password))]
             key[i] = ord(ctmp)
-            state[i]=i
-            i+=1
+            state[i] = i
+            i += 1
 
-    i=j=0
+    i = j = 0
 
     while i < 256:
         j = (j + state[i] + key[i]) % 256
-        swap(state,i,j)
-        i+=1
+        swap(state, i, j)
+        i += 1
 
-    a =1
+    a = 1
     while a <= len(message):
-            i = (i + 1) % 256
-            j = (j + state[i]) % 256
-            swap(state, i, j)
-            k = state[(state[i] + state[j]) % 256]
-            ctmp = message[a-1:a]
-            itmp = ord(ctmp)
-            cipherbyte = itmp ^ k
-            cipherbyte = bytes([cipherbyte]) #change to byte
-            cipher += cipherbyte
-            a += 1
+        i = (i + 1) % 256
+        j = (j + state[i]) % 256
+        swap(state, i, j)
+        k = state[(state[i] + state[j]) % 256]
+        ctmp = message[a - 1:a]
+        itmp = ord(ctmp)
+        cipherbyte = itmp ^ k
+        cipherbyte = bytes([cipherbyte])  # change to byte
+        cipher += cipherbyte
+        a += 1
     return cipher
 
 
-def encrypt(message,password):
-    iv =urandom(10)
+def encrypt(message, password):
+    iv = urandom(10)
     password += iv;
-    cipher = rc4(message,password)
+    cipher = rc4(message, password)
     IVMessage = iv + cipher
     return IVMessage
 
-def decrypt(message,password):
+
+def decrypt(message, password):
     l = message.__len__()
     iv = message[0:10]
     password += iv
@@ -61,26 +63,24 @@ def decrypt(message,password):
     print(len(message))
     return rc4(message, password)
 
-#this is a class
+
+# this is a class
 def log():
-
     return
-#address book
+
+
+# address book
 def addressBook():
-
-
     return
 
 
 def MainScreen():
-
-
-    print("Type a message:")
+    message = str.encode(input("Type a message:"))
+    encryptedMessage = encrypt(message, password)
+    print(decrypt(encryptedMessage,password))
 
 
     return
 
+
 MainScreen()
-    
-
-
